8c8163e409451374070ef402e7c4a08a875b4ea0
gael <76548719+gaelzarco@users.noreply.github.com>
2023-03-13 15:33:18 -0700

    Created trpc posts routes 📝

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,8 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const posts = api.posts.getAll.useQuery();
+  console.log(posts)
 
   return (
     <>
@@ -46,7 +47,6 @@ const Home: NextPage = () => {
           </div>
           <div className="flex flex-col items-center gap-2">
             <p className="text-2xl text-white">
-              {hello.data ? hello.data.greeting : "Loading tRPC query..."}
             </p>
             <AuthShowcase />
           </div>
@@ -61,16 +61,10 @@ export default Home;
 const AuthShowcase: React.FC = () => {
   const { data: sessionData } = useSession();
 
-  const { data: secretMessage } = api.example.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined },
-  );
-
   return (
     <div className="flex flex-col items-center justify-center gap-4">
       <p className="text-center text-2xl text-white">
         {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
       </p>
       <button
         className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,1 +1,34 @@
 import { z } from 'zod'
+
+import { createTRPCRouter, publicProcedure, protectedProcedure } from "~/server/api/trpc";
+
+export const postsRouter = createTRPCRouter({
+    getAll: publicProcedure.query(({ ctx }) => {
+        return ctx.prisma.post.findMany()
+    }),
+
+    getOne: protectedProcedure
+    .input(postSchema)
+    .query(({ ctx, input }) => {
+        return ctx.prisma.post.findUnique({
+        where: {
+            id: input.postId,
+        },
+        });
+    }),
+
+    createOne: protectedProcedure
+    .input(z.object({
+        media: z.string(),
+        body: z.string(),
+    }))
+    .mutation(({ ctx, input }) => {
+        return ctx.prisma.post.create({
+            data: {
+                userId: ctx.session.user.id,
+                media: input.media,
+                body: input.body
+            },
+        })
+    })
+})
