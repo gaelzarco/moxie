db2ba71b9409ef16c46f0e4ab5f9bf809d9dda59
gael <76548719+gaelzarco@users.noreply.github.com>
2023-03-23 21:44:12 -0700

    Futher changes to adopt Clerk ✅

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,13 @@
 import { type AppType } from "next/app";
 import Head from "next/head";
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
 import { ClerkProvider } from "@clerk/nextjs";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
+const MyApp: AppType = ({ Component, pageProps: { ...pageProps } }) => {
   return (
-    <SessionProvider session={session}>
       <ClerkProvider {...pageProps}>
         <Head>
           <title>Moxie</title>
@@ -22,7 +16,6 @@ const MyApp: AppType<{ session: Session | null }> = ({
         </Head>
         <Component {...pageProps} />
       </ClerkProvider>
-    </SessionProvider>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import { type NextPage } from "next";
 import NavBar from "./components/navbar";
-import { signIn, signOut, useSession } from "next-auth/react";
 import { SignInButton, SignOutButton, useUser } from '@clerk/nextjs'
 
 const Home: NextPage = () => {
@@ -32,30 +31,3 @@ const Home: NextPage = () => {
 };
 
 export default Home;
-
-export const AuthShowcase: React.FC = () => {
-  const { data: sessionData } = useSession();
-
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-      </p>
-      {sessionData ? (
-        <button
-        className="w-48 rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={() => void signOut()}
-      >
-        Sign Out
-      </button>
-      ) : (
-        <button
-          className="w-48 rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-          onClick={() => void signIn()}
-          >
-          Get Started
-        </button>
-      )}
-    </div>
-  );
-};
