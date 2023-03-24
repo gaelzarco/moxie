3d274a4043ad99c7b466fd2e4fb055f46933da36
gael <76548719+gaelzarco@users.noreply.github.com>
2023-03-23 22:38:52 -0700

    Removed NextAuth  + posts now rendering 💤

--- src/pages/feed.tsx
+++ src/pages/feed.tsx
@@ -2,13 +2,21 @@ import { type NextPage } from "next"
 import { api } from "~/utils/api";
 
 const Feed: NextPage = () => {
-    const posts = api.posts.getAll.useQuery();
+    const { data } = api.posts.getAll.useQuery();
 
-    console.log(posts)
+    console.log(data)
 
     return (
         <div className='h-screen w-screen bg-black text-white flex flex-col items-center justify-center'>
             <h2>Feed</h2>
+            {data?.map((post) => {
+                return (
+                    <div key={post.id} className='flex flex-col items-center justify-center'>
+                        <h3>{post.body}</h3>
+                        <p>{post.userId}</p>
+                    </div>
+                )
+            })}
         </div>
     )
   }
