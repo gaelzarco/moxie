a9abb5347f7d6d0f9e7cd5f5a0d1f0b8cb9084ee
gael <76548719+gaelzarco@users.noreply.github.com>
2023-03-29 16:22:57 -0700

    Debugging file state w/useffect 💿

--- src/pages/components/createpost.tsx
+++ src/pages/components/createpost.tsx
@@ -1,8 +1,11 @@
+import { useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
 import { 
   type FormEvent, type ChangeEvent,
   useState, useEffect
 } from "react";
+import * as Toast from '@radix-ui/react-toast';
+
 import { api } from "~/utils/api";
 import DragAndDrop from "./draganddrop";
 
@@ -22,6 +25,9 @@ const CreatePost: NextPage = () => {
     })
     const [ file, setFile ] = useState<File | null>(null)
     const [imgView, setImgView] = useState(false)
+    const [open, setOpen] = useState(false);
+
+    const reader = new FileReader()
 
     const { mutate } = api.posts.createOne.useMutation({
       onSuccess: () => {
@@ -35,39 +41,64 @@ const CreatePost: NextPage = () => {
         setPost({...post, body: event.target.value});
       };
       const postFileHandler = (file: File | null) => {
-        setFile(file )
+        setFile(file)
     }
       const handleFormSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault()
+        setOpen(true)
+
+        if (file) {
+          reader.readAsArrayBuffer(file)
+      
+          reader.onload = async () => {
+            const buffer = Buffer.from(reader.result as ArrayBuffer)
+            const base64 = buffer.toString('base64')
+            setPost((prevState) => ({ ...prevState, media: { buffer: base64, mimetype: file.type } }))
+            console.log(post)
+          }
+        }
+
         if (post.body.length > 0) {
-          mutate(post)
+          mutate({
+            body: post.body,
+            media: post.media
+          })
         }
       }
 
       useEffect(() => {
         if (file) {
-          const reader = new FileReader()
-          reader.readAsArrayBuffer(file)
-      
-          reader.onload = () => {
-            const buffer = Buffer.from(reader.result as ArrayBuffer)
-            const base64 = buffer.toString('base64')
-            setPost((prevState) => ({ ...prevState, media: { buffer: base64, mimetype: file.type } }))
-          }
-        } else {
-          setPost((prevState) => ({ ...prevState, media: null }))
+          setFile(file)
+          console.log(file)
         }
       }, [file])
 
       return (
-        <form onSubmit={handleFormSubmit} className="w-auto">
+        <form onSubmit={handleFormSubmit} className="w-full border border-stone-300">
+            <Toast.Provider>
+                <Toast.Root open={open} onOpenChange={setOpen}>
+                  <Toast.Title> Success! </Toast.Title>
+                  <Toast.Description asChild>
+                    <p>
+                      {post.body}
+                    </p>
+                  </Toast.Description>
+                  <Toast.Action asChild altText="Refresh">
+                    <button>Reload</button>
+                  </Toast.Action>
+                  <Toast.Close />
+                </Toast.Root>
+
+                <Toast.Viewport />
+              </Toast.Provider>
+
           <div className="mb-4 w-full">
             <input
               type="text"
               placeholder="What's on your mind?"
               onChange={(event) => postBodyHandler(event)}
               value={post.body}
-              className="border rounded w-full py-2 px-3 text-black active:outline-none focus:outline-none"
+              className="w-full py-2 px-3 text-black active:outline-none focus:outline-none"
             />
           </div>
 
@@ -83,14 +114,14 @@ const CreatePost: NextPage = () => {
                 event.preventDefault()
                 setImgView(!imgView)}
               }
-              className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+              className="rounded-full bg-stone-800/10 px-10 py-3 font-semibold no-underline transition hover:bg-stone-800/20"
             >
                 Attach Image
             </button>
             
             <button
               type="submit"
-              className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+              className="rounded-full bg-stone-800/10 px-10 py-3 font-semibold no-underline transition hover:bg-stone-800/20"
             >
               Submit
             </button>
