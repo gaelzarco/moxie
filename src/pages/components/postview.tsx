52ad576017be85ffe47c075fd87c5fdb6b99445a
gael <76548719+gaelzarco@users.noreply.github.com>
2023-03-30 20:06:01 -0700

    PostView component 👓

---
+++ src/pages/components/postview.tsx
@@ -0,0 +1,57 @@
+import Image from "next/image";
+import { FiHeart, FiMessageCircle, FiShare, FiMoreHorizontal,  } from 'react-icons/fi'
+
+interface PostDataProps {
+    data: {
+        post: {
+            id: string;
+            body: string;
+            media: string | null;
+            link: string | null;
+            createdAt: Date;
+            updatedAt: Date;
+            userId: string;
+        },
+        user: {
+            id: string;
+            userName: string | null;
+            profileImageURL: string;
+        }
+    }[]
+}
+
+const PostView = ({ data }: PostDataProps) => {
+    return (
+        <>
+        {data.map(({ post, user }) => {
+            return (
+                <div key={post.id} className="m-auto text-left border-x border-b border-stone-300 w-full min-w-full p-4">
+                <div className="flex leading-none">
+                    <Image className="rounded-full w-10 h-10" src={user.profileImageURL} height={50} width={50} alt="Profile Picture" />
+                    <div className="pl-2 mb-2 w-full">
+                        <div className=" inline-flex mb-4 w-full justify-between">
+                        <div className="inline-flex justify-start items-center">
+                            <p className="pl-2 font-medium">Gael Zarco</p>
+                            <p className="text-stone-500 text-md hover:cursor-pointer pl-2">@{user.userName === null ? 'username' : user.userName}</p>
+                        </div>
+                        <FiMoreHorizontal className="text-stone-500 hover:cursor-pointer" size={20}/>
+                        </div>
+                        <h4 className="pl-2">{post.body}</h4>
+                        {post.link && (
+                        <Image className="h-auto w-full min-w-full mt-4 rounded-md" src={post.link} height={300} width={500} alt="Attached Media for Post" />
+                        )}
+                        <div className=" inline-flex mt-4 ml-2">
+                        <FiHeart className="hover:cursor-pointer text-stone-500" size={20}/>
+                        <FiMessageCircle className="hover:cursor-pointer text-stone-500 ml-8" size={20}/>
+                        <FiShare className="hover:cursor-pointer text-stone-500 ml-8 align-right" size={20}/>
+                        </div>
+                    </div> 
+                </div> 
+                </div>
+              )
+          })}
+        </>
+    )
+}
+
+export default PostView;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,32 @@
 import { 
   type NextPage,
 } from "next";
-import Image from "next/image";
-import { SignInButton, SignOutButton, useUser } from '@clerk/nextjs'
 import { api } from "~/utils/api";
+import { SignInButton, SignOutButton, useUser } from '@clerk/nextjs'
+import { FiLogOut } from 'react-icons/fi'
+
 import CreatePost from "./components/createpost";
-import { FiHeart, FiMessageCircle, FiShare, FiMoreHorizontal, FiLogOut } from 'react-icons/fi'
+import PostView from "./components/postview";
 
 const Home: NextPage = () => {
   const { isSignedIn } = useUser()
 
   return (
     <>
-        <div className="flex flex-col items-center justify-center m-auto w-screen md:w-5/6 md:max-xl:right-0 md:max-xl:absolute lg:auto xl:w-auto">
+        <div id='home' className="bg-white flex flex-col col-span-2 absolute w-full min-w-750 md:min-w-0 md:max-2xl:w-9/12 md:max-2xl:right-0 2xl:relative">
 
           <div className="sticky top-0 backdrop-blur-lg p-4 w-full m-w-full flex justify-between border-x border-b border-stone-300 hover:cursor-pointer">
             <h2 className="text-1xl font-bold hover:text-stone-700">
               Home
             </h2>
             <div className="text-1xl font-bold inline-flex items-center hover:text-stone-500 hover:cursor-pointer">
-              <FiLogOut className="mr-3 mt-1" size={20}/>
               {!isSignedIn && <SignInButton />}
-              {!!isSignedIn && <SignOutButton />}
+              {!!isSignedIn && (
+                <>
+                  <FiLogOut className="mr-3 mt-1" size={20}/>
+                  <SignOutButton />
+                </>
+              )}
             </div>
 
           </div>
@@ -40,33 +45,9 @@ const Feed = () => {
   if (!data) return <div>Something went wrong...</div>
 
   return (
-      <div className='flex flex-col items-center justify-center w-full min-w-full'>
-          {!!isSignedIn && <CreatePost />}
-          {data.map(({ post, user }) => {
-                return (
-                  <div key={post.id} className="m-auto text-left border-x border-b border-stone-300 w-full min-w-full p-4">
-                    <div className="flex leading-none">
-                      <Image className="rounded-full w-10 h-10" src={user.profileImageURL} height={50} width={50} alt="Profile Picture" />
-                        <div className="pl-2 mb-2 w-full">
-                          <div className=" inline-flex mb-4 w-full">
-                            <p className="pl-2">Gael Zarco</p>
-                            <p className="text-stone-500 pl-2">@{user.userName === null ? 'username' : user.userName}</p>
-                            <FiMoreHorizontal className="text-stone-500 justify-self-end items-end content-end justify-end" size={20}/>
-                          </div>
-                          <h4 className="pl-2">{post.body}</h4>
-                          {post.link && (
-                            <Image className="h-auto w-full min-w-full mt-4" src={post.link} height={300} width={500} alt="Attached Media for Post" />
-                          )}
-                          <div className=" inline-flex mt-4 ml-2">
-                            <FiHeart className="text-stone-500" size={20}/>
-                            <FiMessageCircle className="text-stone-500 ml-8" size={20}/>
-                            <FiShare className="text-stone-500 ml-8 align-right" size={20}/>
-                          </div>
-                        </div> 
-                    </div> 
-                  </div>
-              )
-          })}
+      <div className='flex flex-col items-center justify-center'>
+        {!!isSignedIn && <CreatePost />}
+        <PostView data={data} />
       </div>
     )
   }
