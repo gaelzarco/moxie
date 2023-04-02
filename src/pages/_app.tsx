53aadc5a00ae374809cb8b6a66693632c59c4fcc
gael <76548719+gaelzarco@users.noreply.github.com>
2023-04-01 20:40:48 -0700

    Refactoring Toaster

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,13 @@ import { type AppType } from "next/app";
 import Head from "next/head";
 import { Plus_Jakarta_Sans } from 'next/font/google';
 import { ClerkProvider } from "@clerk/nextjs";
-
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
+
 import NavBar from "./components/navbar";
 import SideBar from "./components/sidebar";
+
+import * as Toast from '@radix-ui/react-toast';
 
 const jakartaSans = Plus_Jakarta_Sans({
   subsets: [ "latin" ],
@@ -23,6 +25,7 @@ const MyApp: AppType = ({ Component, pageProps: { ...pageProps } }) => {
           <meta name="description" content="Blazingly fast, secure, and versatile. Meet the next generation of social media." />
           <link rel="icon" href="/favicon.ico" />
         </Head>
+        <Toast.Provider swipeDirection="right">
         <main className={`${jakartaSans.variable} font-sans h-auto mx-auto grid grid-cols-4 max-w-screen-2xl`}>
           <div className="h-screen bg-white col-span-1">
           <NavBar />
@@ -34,6 +37,7 @@ const MyApp: AppType = ({ Component, pageProps: { ...pageProps } }) => {
           <SideBar />
           </div>
         </main>
+        </Toast.Provider>
       </ClerkProvider>
   );
 };
