e951a8728453b9417f74e5a98b176d3227a6f8fd
gael <76548719+gaelzarco@users.noreply.github.com>
2023-04-03 14:43:31 -0700

    Displays like + comment metrics & replies view 🤩

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -5,18 +5,21 @@ import { generateSSGHelper } from "~/server/helpers/ssgHelper";
 
 import CreatePost from "../components/createpost";
 import PostView from "../components/postview";
+import RepliesView from "../components/repliesview";
 
-const Post: NextPage<{ id: string }> = ({ id }) => {
-    const { data, isLoading } = api.posts.getOne.useQuery(id)
+const Post: NextPage<{ postId: string }> = ({ postId }) => {
+    const postQuery = api.posts.getOneById.useQuery(postId)
+    const replyQuery = api.replies.getAllByPostId.useQuery(postId)
     const { isSignedIn } = useUser()
 
-    if (isLoading) return <div>Loading...</div>
-    if (!data) return <div>Something went wrong...</div>
+    if (postQuery.isLoading || replyQuery.isLoading) return <div>Loading...</div>
+    if (!postQuery.data || !replyQuery.data) return <div>Something went wrong...</div>
 
     return (
         <div className="h-auto min-h-screen bg-white min-w-750 md:max-2xl:w-9/12 md:max-2xl:right-0 max-2xl:absolute border-b border-x border-stone-300">
-            {!!data && <PostView {...data} />}
-            {!!isSignedIn && <CreatePost />}
+            {!!postQuery.data && <PostView {...postQuery.data} />}
+            {!!isSignedIn && <CreatePost postId={postId} reply/>}
+            {!!replyQuery.data && <RepliesView {...replyQuery.data} />}
         </div>
     )
 }
@@ -24,16 +27,16 @@ const Post: NextPage<{ id: string }> = ({ id }) => {
 export const getStaticProps: GetStaticProps = async (context) => {
     const ssg = generateSSGHelper()
 
-    const id = context.params?.id
+    const postId = context.params?.id
 
-    if (typeof id !== 'string') throw new Error ('Invalid post id')
+    if (typeof postId !== 'string') throw new Error ('Invalid post ID')
 
-    await ssg.posts.getOne.prefetch(id)
+    await ssg.posts.getOneById.prefetch(postId)
 
     return {
         props: {
             trpcState: ssg.dehydrate(),
-            id
+            postId
         }
     }
 }
