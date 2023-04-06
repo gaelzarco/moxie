5f5f2c0188fc2fa101ceea84d2ed03a68c5cf596
gael <76548719+gaelzarco@users.noreply.github.com>
2023-04-05 17:13:39 -0700

    New toast component 🍞

---
+++ src/pages/components/toast.tsx
@@ -0,0 +1,32 @@
+import { NextPage } from "next";
+import { type ReactNode, useRef } from "react";
+import * as ToastPrimitive from '@radix-ui/react-toast';
+
+const ToastComponent: NextPage<{title: string, content?: string, children: ReactNode}> = (
+    { title, children }: {title: string, children: ReactNode}) => {
+
+    const toastDateRef = useRef(new Date())
+
+    return (
+        <ToastPrimitive.Provider swipeDirection="right">
+            <ToastPrimitive.Root className="ToastRoot">
+                <ToastPrimitive.Title className="ToastTitle">{title}</ToastPrimitive.Title>
+                <ToastPrimitive.Description asChild>
+                    <time className="ToastDescription" dateTime={toastDateRef.current.toISOString()}>
+                        {toastDate(toastDateRef.current)}
+                    </time>
+                </ToastPrimitive.Description >
+                <ToastPrimitive.Action className="ToastAction" asChild altText="Close">
+                    {children}
+                </ToastPrimitive.Action >
+            </ToastPrimitive.Root>
+            <ToastPrimitive.Viewport className="ToastViewport"/>
+        </ToastPrimitive.Provider>
+    )
+}
+
+export function toastDate(date: Date) {
+    return new Intl.DateTimeFormat('en-US', { dateStyle: 'full', timeStyle: 'short' }).format(date);
+  }
+
+export default ToastComponent;
