6693be67d8f0b62c246b6db31cb48cc563fa385a
gael <76548719+gaelzarco@users.noreply.github.com>
2023-04-07 16:28:20 -0700

    Removed users router from root appRouter ❌

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,17 +1,10 @@
 import { createTRPCRouter } from "~/server/api/trpc";
 import { postsRouter } from "~/server/api/routers/posts";
-import { usersRouter } from "~/server/api/routers/users";
 import { repliesRouter } from "~/server/api/routers/replies";
 import { likesRouter } from "~/server/api/routers/likes";
 
-/**
- * This is the primary router for your server.
- *
- * All routers added in /api/routers should be manually added here.
- */
 export const appRouter = createTRPCRouter({
   posts: postsRouter,
-  users: usersRouter,
   replies: repliesRouter,
   likes: likesRouter
 });
