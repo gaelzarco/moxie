218f7e711b41e46b29a11b09036f71026db9ddfa
gael <76548719+gaelzarco@users.noreply.github.com>
2023-04-11 17:46:11 -0700

    Fixed links 🔗✅

--- src/pages/components/postview.tsx
+++ src/pages/components/postview.tsx
@@ -71,7 +71,7 @@ const PostView: NextPage<PostWithUserAndImage> = ( data ) => {
                             <Share1Icon 
                                 className="hover:cursor-pointer dark:text-white ml-20 h-5 w-5 align-right"
                                 onClick={() => {
-                                    navigator.clipboard.writeText(`https://moxie-x.vercel.app/${user.userName as string}/post/${post.id as string}`)
+                                    navigator.clipboard.writeText(`https://moxie-x.vercel.app/post/${post.id && post.id}`)
                                     .then(toastHandler)
                                     .catch((err) => console.log(err))
                                 }} 
