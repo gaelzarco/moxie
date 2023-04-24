cc40dfd3576d839c3f3aeae63cc4f9a7f99f500c
gael <76548719+gaelzarco@users.noreply.github.com>
2023-04-24 16:45:43 -0700

    Implemented image loader 🔃

--- src/pages/components/aspectratioimage.tsx
+++ src/pages/components/aspectratioimage.tsx
@@ -1,19 +1,31 @@
 import type { NextPage } from "next";
+import { useState } from "react";
 import Image from "next/image";
 import * as AspectRatioPrimitive from '@radix-ui/react-aspect-ratio';
+import { Jelly } from "@uiball/loaders";
 
 const ApectRatioImage: NextPage<{ src: string, alt: string }> = ({ src, alt }) => {
-    
+    const [isLoading, setIsLoading] = useState(true)
+
+    const handleImageLoad = () => {
+        setIsLoading(false);
+      }
+
     return (
-        <div className="h-auto w-full min-w-full mb-4 rounded-3xl overflow-hidden shadow-lg">
+        <div className="h-auto w-full mb-4 rounded-3xl overflow-hidden shadow-xl">
             <AspectRatioPrimitive.Root  ratio={16 / 9}>
+                {isLoading &&  (
+                    <div className="h-full w-full flex items-center justify-center content-center">
+                        <Jelly color="white" size={15} />
+                    </div>
+                )}
                 <Image 
-                    className="object-cover w-full h-full"
+                    className="object-cover h-full w-full"
                     src={src}
                     alt={alt}
-                    width={500}
                     height={300}
-                    priority
+                    width={500}
+                    onLoad={handleImageLoad}
                 />
             </AspectRatioPrimitive.Root>
         </div>
