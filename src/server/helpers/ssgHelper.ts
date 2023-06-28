490508672b8587f96f8c9b077a299951736a143e
gael <76548719+gaelzarco@users.noreply.github.com>
2023-06-27 17:48:49 -0700

    ssg fix

--- src/server/helpers/ssgHelper.ts
+++ src/server/helpers/ssgHelper.ts
@@ -6,6 +6,6 @@ import superjson from "superjson";
 export const generateSSGHelper = () =>
   createProxySSGHelpers({
     router: appRouter,
-    ctx: { prisma, userId: null, userName: null },
+    ctx: { prisma, userId: null },
     transformer: superjson, // optional - adds superjson serialization
   });
