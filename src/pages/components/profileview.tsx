d9313dfdb41367ef7e6470915fda52ba14eaedff
gael <76548719+gaelzarco@users.noreply.github.com>
2023-07-14 06:31:06 -0700

    missed a spot

--- src/pages/components/profileview.tsx
+++ src/pages/components/profileview.tsx
@@ -18,9 +18,9 @@ const ProfileView: NextPage< Profile > = ({ filteredUser, postCount, replyCount 
     return (
         <>
         {(!!filteredUser)  && (
-            <div key={filteredUser.id} className="cursor-default mx-auto text-left w-11/12 p-5 rounded-xl mt-5 dark:text-white dark:bg-neutral-900">
+            <div key={filteredUser.id} className="shadow-xl dark:shadow-none cursor-default mx-auto text-left w-11/12 p-5 rounded-xl mt-5 dark:text-white bg-neutral-100 dark:bg-neutral-900">
                 <div className="flex flex-col pl-5 mt-5 mb-4">
-                    <Image className="m-2 rounded-full w-24 h-24 bg-neutral-800 cursor-pointer" src={filteredUser.profileImageURL} height={50} width={50} alt="Profile Picture" />
+                    <Image className="my-2 rounded-full w-24 h-24 bg-neutral-800" src={filteredUser.profileImageURL} height={50} width={50} alt="Profile Picture" />
                     <div className="flex flex-col p-2 mb-1 w-full">
                         <p className="text-2xl font-semibold leading-10">{filteredUser.firstName}{' '}{filteredUser.lastName}</p>
                         <p className="text-stone-500 text-md">@{!filteredUser.userName ? 'username' : filteredUser.userName}</p>
